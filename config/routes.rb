d67022b76a81f854be4f6d301f81d315b7500649
Steve <sntakirutimana72@gmail.com>
2022-10-11 08:46:01 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'home#splash'
+  root 'splash#index'
 
   devise_for :users
+
+  resources :categories, only: %i[index create new] do
+    resources :entities, only: %i[index new create]
+  end
 end
