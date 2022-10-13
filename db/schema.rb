af12344e43455e71aafb70e21085e0141a7c557a
Steve <sntakirutimana72@gmail.com>
2022-10-14 00:07:43 +0200

    Fix linter offenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_133954) do
+ActiveRecord::Schema[7.0].define(version: 20_221_010_133_954) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_categories_on_author_id"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon'
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_categories_on_author_id'
   end
 
-  create_table "categorizations", id: false, force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "entity_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_categorizations_on_category_id"
-    t.index ["entity_id"], name: "index_categorizations_on_entity_id"
+  create_table 'categorizations', id: false, force: :cascade do |t|
+    t.bigint 'category_id', null: false
+    t.bigint 'entity_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_categorizations_on_category_id'
+    t.index ['entity_id'], name: 'index_categorizations_on_entity_id'
   end
 
-  create_table "entities", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "amount", null: false
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_entities_on_author_id"
+  create_table 'entities', force: :cascade do |t|
+    t.string 'name', null: false
+    t.decimal 'amount', null: false
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_entities_on_author_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "categories", "users", column: "author_id"
-  add_foreign_key "categorizations", "categories"
-  add_foreign_key "categorizations", "entities"
-  add_foreign_key "entities", "users", column: "author_id"
+  add_foreign_key 'categories', 'users', column: 'author_id'
+  add_foreign_key 'categorizations', 'categories'
+  add_foreign_key 'categorizations', 'entities'
+  add_foreign_key 'entities', 'users', column: 'author_id'
 end
